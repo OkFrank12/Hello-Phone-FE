20d58b48b88d5b4921c7d4b0af65fcd3a4d9d8e6
OkFrank12 <cfoonyemmemme@gmail.com>
2023-10-25 23:21:18 +0100

    updates

--- src/pages/home/HomeContacts.tsx
+++ src/pages/home/HomeContacts.tsx
@@ -5,7 +5,7 @@ import { BiSolidContact } from "react-icons/bi";
 import moment from "moment";
 
 const HomeContacts = () => {
-  const { data } = useReadAllContacts();
+  const { data, isLoading } = useReadAllContacts();
   return (
     <div>
       <div className="w-full flex flex-col bg-gradient-to-b from-red-600 to-orange-400 min-h-[100vh]">
@@ -24,37 +24,45 @@ const HomeContacts = () => {
         </div>
         <div className="w-full flex flex-col items-center flex-wrap min-h-[300px]">
           <div className="flex w-full justify-center mt-3 items-center flex-wrap">
-            {data?.map((el: any) => {
-              return (
-                <div
-                  key={el?._id}
-                  className="w-[500px] smallPhone:p-1  tablet:w-[67%] smallPhone:w-[80%] m-1 h-[80px] rounded-md shadow-3xl flex items-center justify-between p-4 bg-white"
-                >
-                  <div className="w-[60px] text-[30px] tablet:text-[20px] tablet:w-[40px] tablet:h-[40px] flex justify-center items-center text-white rounded-full h-[60px] bg-gradient-to-b from-red-600 to-orange-400">
-                    <BiSolidContact />
-                  </div>
-                  <div className="flex flex-col text-center flex-1 items-center">
-                    <p className="font-bold text-rose-800 tablet:text-[12px] tablet:capitalize uppercase">
-                      {el?.contactName}
-                    </p>
-                    <span className="text-[12px] tablet:text-[10px] tablet:flex flex-col items-center">
-                      <span className="mx-2">
-                        <span className="text-rose-700 capitalize">tel:</span>{" "}
-                        {el?.contactNumber}
-                      </span>
-                      <div className="w-[5px] h-[5px] bg-orange-500"></div>
-                      {el?.category}
-                      <span className="text-rose-400 ml-2">
-                        {moment(el?.createdAt).fromNow()}
-                      </span>
-                    </span>
-                  </div>
-                  <div className="text-[30px] w-[50px] smallPhone:h-[30px] smallPhone:w-[30px] smallPhone:text-[20px] h-[50px] flex justify-center hover:bg-orange-500 hover:text-white text-orange-800 transition-all duration-300 cursor-pointer items-center rounded-full">
-                    {el?.contactName.charAt(0)}
-                  </div>
-                </div>
-              );
-            })}
+            {isLoading ? (
+              <div>Fetching Contacts</div>
+            ) : (
+              <>
+                {data?.map((el: any) => {
+                  return (
+                    <div
+                      key={el?._id}
+                      className="w-[500px] smallPhone:p-1  tablet:w-[67%] smallPhone:w-[80%] m-1 h-[80px] rounded-md shadow-3xl flex items-center justify-between p-4 bg-white"
+                    >
+                      <div className="w-[60px] text-[30px] tablet:text-[20px] tablet:w-[40px] tablet:h-[40px] flex justify-center items-center text-white rounded-full h-[60px] bg-gradient-to-b from-red-600 to-orange-400">
+                        <BiSolidContact />
+                      </div>
+                      <div className="flex flex-col text-center flex-1 items-center">
+                        <p className="font-bold text-rose-800 tablet:text-[12px] tablet:capitalize uppercase">
+                          {el?.contactName}
+                        </p>
+                        <span className="text-[12px] tablet:text-[10px] tablet:flex flex-col items-center">
+                          <span className="mx-2">
+                            <span className="text-rose-700 capitalize">
+                              tel:
+                            </span>
+                            {el?.contactNumber}
+                          </span>
+                          <div className="w-[5px] h-[5px] bg-orange-500"></div>
+                          {el?.category}
+                          <span className="text-rose-400 ml-2">
+                            {moment(el?.createdAt).fromNow()}
+                          </span>
+                        </span>
+                      </div>
+                      <div className="text-[30px] w-[50px] smallPhone:h-[30px] smallPhone:w-[30px] smallPhone:text-[20px] h-[50px] flex justify-center hover:bg-orange-500 hover:text-white text-orange-800 transition-all duration-300 cursor-pointer items-center rounded-full">
+                        {el?.contactName.charAt(0)}
+                      </div>
+                    </div>
+                  );
+                })}
+              </>
+            )}
           </div>
         </div>
       </div>
